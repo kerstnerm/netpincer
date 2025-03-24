2a6ee64fcb436c2a54850c9ebc49972b674e1af3
Mate Kerstner <kerstner.mate@mik.uni-pannon.hu>
2025-03-24 15:31:43 +0100

    Fix create endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,7 +10,7 @@ const Food = {
     getFoods: () => axiosInstance.get<IFood[]>(`/api/food`),
     getFood: (id: string) => axiosInstance.get<IFood>(`/api/food/${id}`),
     getCategories: () => axiosInstance.get<{name: string, id: number}[]>(`/api/food/categories`),
-    createFood : (param: ICreateFood) => axiosInstance.post<IFood>(`/api/food/create`, param),
+    createFood : (param: ICreateFood) => axiosInstance.post<IFood>(`/api/food`, param),
     updateFood: (id: string, param2: {
         foodCategoryId: number;
         price: number;
